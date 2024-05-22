6e6e4c08ba401e26d6ed5b370cde8ba468891a9f
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-05-22 18:42:33 -0300

    feat: create `print` method

--- c/dynamic_array.c
+++ c/dynamic_array.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct array {
+struct array
+{
     int capacity;
     int size;
     int *arr;
 }
 
 push(int value, struct array *v) {
-    if (v->size == v->capacity) {
+    if (v->size == v->capacity)
+    {
         v->capacity = 2 * v->capacity;
-        v->arr = (void *)realloc(v->arr, v->capacity);        
+        v->arr = (void *)realloc(v->arr, v->capacity);
     }
     v->arr[v->size] = value;
-    v->size++; 
+    v->size++;
+}
+
+print(struct array *v) {
+    for (int i = 0; i < v->capacity; i++) {
+        printf("%d ", *(v->arr + i));
+    }
 }
 
 int main(void) {
