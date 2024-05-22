c234a3ce942b391ecbef04ce4186ea5ec19cb37c
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-05-22 19:01:59 -0300

    feat: implement method `get` to retrieve value by index from array

--- c/dynamic_array.c
+++ c/dynamic_array.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct array
-{
+struct array {
     int capacity;
     int size;
     int *arr;
 }
 
 push(int value, struct array *v) {
-    if (v->size == v->capacity)
-    {
+    if (v->size == v->capacity) {
         v->capacity = 2 * v->capacity;
         v->arr = (void *)realloc(v->arr, v->capacity);
     }
@@ -18,10 +16,21 @@ push(int value, struct array *v) {
     v->size++;
 }
 
-print(struct array *v) {
+show(struct array *v) {
+    printf("Capacity: %d ", v->capacity);
+    printf("Size: %d ", v->size);
+    printf("Values: ");
     for (int i = 0; i < v->capacity; i++) {
         printf("%d ", *(v->arr + i));
     }
+    printf("\n");
+}
+
+int get(int index, struct array *v) {
+    if (index < 0 || index >= v->capacity) {
+        return -1;
+    }
+    return *(v->arr+index);
 }
 
 int main(void) {
@@ -29,7 +38,16 @@ int main(void) {
     v.capacity = 2;
     v.size = 0;
     v.arr = (int *)calloc(v.capacity, sizeof(int));
-
+    push(12, &v);
+    push(13, &v);
+    push(14, &v);
+    show(&v);
+    int val = get(1, &v);
+    if (val == -1) {
+        printf("\nIndex out of range, response: %d\n", val);
+    } else {
+        printf("\nGetted value: %d\n", val);
+    }
     free(v.arr);
     return 0;
 }
