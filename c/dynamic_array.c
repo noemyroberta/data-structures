f75bf238516846d74fe8dd5c7a6592b3b901cee0
Noemy Roberta <noemy.oliveira2@outlook.com>
2024-05-23 20:30:14 -0300

    feat: add method `insert` to insert a value at the index of array

--- c/dynamic_array.c
+++ c/dynamic_array.c
@@ -2,82 +2,90 @@
 #include <stdlib.h>
 
 struct array {
-    int capacity;
-    int size;
-    int *arr;
+  int capacity;
+  int size;
+  int *arr;
 };
 
 void push(int value, struct array *v) {
-    if (v->size == v->capacity) {
-        v->capacity = 2 * v->capacity;
-        v->arr = (void *)realloc(v->arr, v->capacity);
+  if (v->size == v->capacity) {
+    v->capacity = 2 * v->capacity;
+    v->arr = (void *)realloc(v->arr, v->capacity);
+  }
+  v->arr[v->size] = value;
+  v->size++;
+}
+
+int insert(int index, int value, struct array *v) {
+  if (v->size == v->capacity) {
+    return -1;
+  } else if (*(v->arr + index) != 0) {
+    for (size_t i = v->capacity; i > index; i--) {
+      v->arr[i] = v->arr[i - 1];
     }
-    v->arr[v->size] = value;
-    v->size++;
+  }
+  v->arr[index] = value;
+  v->size++;
+  return 0;
 }
 
-int isEmpty(struct array *v) {
-    return v->size == 0;
-}
+int isEmpty(struct array *v) { return v->size == 0; }
 
-int capacity(struct array *v) {
-    return v->capacity;
-}
+int capacity(struct array *v) { return v->capacity; }
 
-int size(struct array *v) {
-    return v->size;
-}
+int size(struct array *v) { return v->size; }
 
 void show(struct array *v) {
-    printf("Capacity: %d ", v->capacity);
-    printf("Size: %d ", v->size);
-    printf("Values: ");
-    for (int i = 0; i < v->capacity; i++) {
-        printf("%d ", *(v->arr + i));
-    }
-    printf("\n");
+  printf("Capacity: %d ", v->capacity);
+  printf("Size: %d ", v->size);
+  printf("Values: ");
+  for (int i = 0; i < v->capacity; i++) {
+    printf("%d ", *(v->arr + i));
+  }
+  printf("\n");
 }
 
 int pop(struct array *v) {
-    if (isEmpty(v)) {
-        return -1;
-    }
-    int lastIndex = v->size-1;
-    int lastValue = *(v->arr+lastIndex); 
-    *(v->arr+lastIndex) = 0;
-    v->size--;
-    return lastValue;
+  if (isEmpty(v)) {
+    return -1;
+  }
+  int lastIndex = v->size - 1;
+  int lastValue = *(v->arr + lastIndex);
+  *(v->arr + lastIndex) = 0;
+  v->size--;
+  return lastValue;
 }
 
 int at(int index, struct array *v) {
-    if (index < 0 || index >= v->capacity) {
-        return -1;
-    }
-    return *(v->arr+index);
+  if (index < 0 || index >= v->capacity) {
+    return -1;
+  }
+  return *(v->arr + index);
 }
 
 int main(void) {
-    struct array v;
-    v.capacity = 2;
-    v.size = 0;
-    v.arr = (int *)calloc(v.capacity, sizeof(int));
-    push(12, &v);
-    //push(13, &v);
-    //push(14, &v);
-    show(&v);
-    int val = at(1, &v);
-    if (val == -1) {
-        printf("\nIndex out of range, response: %d\n", val);
-    } else {
-        printf("\nGetted value: %d\n", val);
-    }
-    int lastValue = pop(&v);
-    if (lastValue == -1) {
-        printf("The heap is empty!");
-    } else {
-        printf("Last value: %d", lastValue);
-    }
-    show(&v);
-    free(v.arr);
-    return 0;
+  struct array v;
+  v.capacity = 2;
+  v.size = 0;
+  v.arr = (int *)calloc(v.capacity, sizeof(int));
+  push(12, &v);
+  push(13, &v);
+  push(14, &v);
+  show(&v);
+  int result = insert(1, 43, &v);
+  //   int val = at(1, &v);
+  //   if (val == -1) {
+  //     printf("\nIndex out of range, response: %d\n", val);
+  //   } else {
+  //     printf("\nGetted value: %d\n", val);
+  //   }
+  //   int lastValue = pop(&v);
+  //   if (lastValue == -1) {
+  //     printf("The heap is empty!");
+  //   } else {
+  //     printf("Last value: %d", lastValue);
+  //   }
+  show(&v);
+  free(v.arr);
+  return 0;
 }
